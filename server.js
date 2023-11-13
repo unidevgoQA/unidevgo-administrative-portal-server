b9660db748515b5c882dc614183b0d5cd2bf469b
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2023-11-13 15:18:32 +0600

    Added update leave status API

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clusterunidevgo.2nk4dzo.mongodb.net/?retryWrites=true&w=majority"`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clusterunidevgo.2nk4dzo.mongodb.net/?retryWrites=true`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -52,28 +52,19 @@ const run = async () => {
     //Add Profile
     app.post("/profile", async (req, res) => {
       const profile = req.body;
-      console.log(profile);
-      const result = await profileCollection.insertOne(profile, {
-        writeConcern: { w: 0 },
-      });
+      const result = await profileCollection.insertOne(profile);
       res.send({ status: true, data: result });
     });
     //Add work status
     app.post("/work-status", async (req, res) => {
       const singleWorkStatus = req.body;
-      console.log(singleWorkStatus);
-      const result = await workStatusCollection.insertOne(singleWorkStatus, {
-        writeConcern: { w: 0 },
-      });
+      const result = await workStatusCollection.insertOne(singleWorkStatus);
       res.send({ status: true, data: result });
     });
     //Add leave management
     app.post("/leave-management", async (req, res) => {
       const leaveApply = req.body;
-      console.log(leaveApply);
-      const result = await leaveManagementCollection.insertOne(leaveApply, {
-        writeConcern: { w: 0 },
-      });
+      const result = await leaveManagementCollection.insertOne(leaveApply);
       res.send({ status: true, data: result });
     });
     //Get profile by email
@@ -88,54 +79,60 @@ const run = async () => {
     //Delete single item from work status Api
     app.delete("/work-status/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: new ObjectId(id) };
-      const result = await workStatusCollection.deleteOne(query, {
-        writeConcern: { w: 0 },
-      });
+      const result = await workStatusCollection.deleteOne(query);
       res.json({ status: true, data: result });
     });
     //Delete single item from leave-management Api
     app.delete("/leave-management/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await leaveManagementCollection.deleteOne(query, {
-        writeConcern: { w: 0 },
-      });
+      const result = await leaveManagementCollection.deleteOne(query);
       res.json({ status: true, data: result });
     });
     //Delete single item from profile Api
     app.delete("/profile/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await profileCollection.deleteOne(query, {
-        writeConcern: { w: 0 },
-      });
+      const result = await profileCollection.deleteOne(query);
       res.json({ status: true, data: result });
     });
     //Update Work Status
     app.put("/work-status/:id", async (req, res) => {
       const id = req.params.id;
       const updatedTaskStatus = req.body;
-
-      console.log(id, updatedTaskStatus);
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateTask = {
         $set: {
-          workStatus:updatedTaskStatus.workStatus,      
+          workStatus: updatedTaskStatus.workStatus,
         },
       };
       const result = await workStatusCollection.updateOne(
         filter,
         updateTask,
-        options,{
-          writeConcern: { w:0}
-        }
+        options
       );
       res.send({ status: true, data: result });
     });
-
+    //Update leave Status
+    app.put("/leave-management/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedLeaveStatus = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateTask = {
+        $set: {
+          status: updatedLeaveStatus.leaveStatus,
+        },
+      };
+      const result = await leaveManagementCollection.updateOne(
+        filter,
+        updateTask,
+        options
+      );
+      res.send({ status: true, data: result });
+    });
   } finally {
   }
 };
