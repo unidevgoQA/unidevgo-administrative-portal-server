598f5d635263dae2ae9f430782da38ba141704dd
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2024-01-08 14:59:55 +0600

    Updated

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 //Middleware
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
